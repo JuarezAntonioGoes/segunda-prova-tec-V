50605382ad930f37f0f3fd7bbde15ba33a0501e5
Juarez Junior <juarezantoniog12@gmail.com>
2021-06-29 20:03:05 -0300

    refac form cadastro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,16 +8,9 @@ import { CadastroComponent } from './cadastro/cadastro.component';
 import { ListarComponent } from './listar/listar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CadastroComponent,
-    ListarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CadastroComponent, ListarComponent],
+  imports: [FormsModule, BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
